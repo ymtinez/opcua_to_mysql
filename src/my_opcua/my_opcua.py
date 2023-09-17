ddf5a3bb5b8dd3ddffbbfb3678a987f8b04f1bb5
Yunieski Martinez Espinosa <ymtinez@gmail.com>
2023-09-17 23:36:06 +0200

    Update my_opcua.py
    Refactoring variable type handling for arrays and removing unused functions
    
    Previously, when a variable had an array type, the value was an array of values. Now, the code creates individual variables for each element of the array.
    
    Removed the following unused functions:
    - "get_variable_type(self, node_id: str) -> str"
    - "get_db_variables_type(self, db_node_id: str) -> dict"

--- src/my_opcua/my_opcua.py
+++ src/my_opcua/my_opcua.py
@@ -8,46 +8,6 @@ class MyOPCUA:
     def __init__(self, url: str) -> None:
         self.client = Client(url)
 
-    def get_variable_type(self, node_id: str) -> str:
-        """This function return the type of the specific variable from OPCUA-Server
-
-        Args:
-            node_id (string): Node identification: "ns=<namespaceIndex>;s=<stringIdentifier>"
-
-        Returns:
-            str: "Real" if Type.float, "Int" if Type.int, "Boolean" if Type.bool, "" if not exist
-        """
-        client_node = self.client.get_node(node_id)  # get node
-        client_node_value = client_node.get_value()
-        if isinstance(client_node_value, float):
-            return "Real"
-        elif isinstance(client_node_value, bool):
-            return "Boolean"
-        elif isinstance(client_node_value, int):
-            return "Int"
-        else:
-            return ""
-
-    def get_db_variables_type(self, db_node_id: str) -> dict:
-        """This function return a dictionay with the name and respetive type of each variables into de specific database
-
-        Args:
-            db_name (str): name of the specific DataBase
-
-        Returns:
-            dict: {"var_name": "var_type"}
-        """
-        var_type_temp = {}
-        if db_node_id != "":
-            db_node = self.client.get_node(db_node_id)
-            variables = db_node.get_children()
-            for variable in variables:
-                variable_type = self.get_variable_type(variable)
-                client_node = self.client.get_node(variable)
-                client_node_name = str(client_node.get_browse_name())[16:-1]
-                var_type_temp.update({f"{client_node_name}": variable_type})
-        return var_type_temp
-
     def get_input_value(self, node_id: str) -> dict:
         """This function return a dictionary with the name and respetive value of the specific variable from OPCUA-Server
 
@@ -57,15 +17,26 @@ class MyOPCUA:
         Returns:
             dict: {"var_name": "var_value"}
         """
+        dict_temp = {}
         client_node = self.client.get_node(node_id)  # get node
         client_node_value = client_node.get_value()  # read node value
-        client_node_value_type = self.get_variable_type(node_id)
         client_node_name = str(client_node.get_browse_name())[16:-1]
-        return {
-            f"{client_node_name}": client_node_value
-            if client_node_value_type != "Real"
-            else round(client_node_value, 2)
-        }
+        if isinstance(client_node_value, list):
+            for index, value in enumerate(client_node_value):
+                dict_temp.update(
+                    {
+                        f"{client_node_name}[{index}]": value
+                        if not isinstance(value, float)
+                        else round(client_node_value, 2)
+                    }
+                )
+            return dict_temp
+        else:
+            return {
+                f"{client_node_name}": client_node_value
+                if not isinstance(client_node_value, float)
+                else round(client_node_value, 2)
+            }
 
     def get_specific_db_node_id(self, db_name: str) -> str:
         """This function return the node id of the specific DataBase
